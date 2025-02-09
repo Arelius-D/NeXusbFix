ace41341468f1322de17966417d9c0eef2ad0eae
Arelius-D <168124713+Arelius-D@users.noreply.github.com>
2025-02-09 02:20:33 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "[INFO] Installing NeXusbFix..."
-sudo cp scripts/nexusbfix.sh /usr/local/bin/nexusbfix
-sudo chmod +x /usr/local/bin/nexusbfix
-echo "[INFO] Installation complete! Run 'nexusbfix' to start."
